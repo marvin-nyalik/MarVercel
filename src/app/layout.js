6aa4943b04ae497af9b840b1acc8b8a3a71c8dc3
MARVIN <o.silverygold@gmail.com>
2024-01-24 21:34:01 +0300

    Update layout and Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Binyam-Portfolio',
-  description: 'My Personal Portfolio Website',
+  title: 'Marvin-Portfolio',
+  description: 'Marv Professional Portfolio',
 }
 
 export default function RootLayout({ children }) {
